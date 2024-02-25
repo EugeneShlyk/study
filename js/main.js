54d13cc850bc6a2bd7be26e0f8d207fb710c9912
Evgene Shlyk <serpupstars@gmail.com>
2024-02-26 07:24:51 +1000

    26 февраля

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+let button = document.querySelector('.penis');
+let button2 = document.querySelector('.vagina');
+let div = document.querySelector('.out');
 
+
+
+button.addEventListener('click', () => {
+  div.textContent = 'хуй';
+  let a = document.querySelector('.i-1').value;
+  console.log(a);
+})
+
+button2.addEventListener('click', () => {
+  div.textContent = 'пизда';
+  let a = document.querySelector('.i-1').value;
+  console.log(a);
+})
 
 
