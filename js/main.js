ecc03c56eec1bb911c5205afd00c32187fed1852
Evgene Shlyk <serpupstars@gmail.com>
2024-02-29 07:26:37 +1000

    29 февраля утро

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 let boxes = document.querySelectorAll('.box');
+let circles = document.querySelectorAll('.circle');
+
+circles[0].addEventListener('click', (evt) => {
+  boxes[1].append(evt.cloneNode(true));
+})
+
+
 
 
 
