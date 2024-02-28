46f637086de1a3d22512fd13663bd1fd90026054
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2024-02-28 16:45:15 +1000

    28 февраля работа

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
-let button = document.querySelector('.penis');
-let button2 = document.querySelector('.vagina');
-let div = document.querySelector('.out');
+let boxes = document.querySelectorAll('.box');
 
 
 
-button.addEventListener('click', () => {
-  div.textContent = 'хуй';
-  let a = document.querySelector('.i-1').value;
-  console.log(a);
-})
-
-button2.addEventListener('click', () => {
-  div.textContent = 'пизда';
-  let a = document.querySelector('.i-1').value;
-  console.log(a);
-})
 
 
