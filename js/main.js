278dfa2abd73b4adf502fdd1f32dca57338dacc8
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2024-03-04 17:42:07 +1000

    4 марта вечер

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-let boxes = document.querySelectorAll('.box');
+let buttons = document.querySelectorAll('.button');
 let circles = document.querySelectorAll('.circle');
-let templateFragment = document.querySelector('#template').content;
-let template = templateFragment.querySelector('div');
-let fragment = document.createDocumentFragment();
-console.log(boxes[0]);
 
-for (let i = 0; i < 6; i++) {
+let handleClick = (event) => {
+  // console.log(parseInt(event.currentTarget.textContent));
+  let numberButton = event.currentTarget.textContent;
+  // console.log(numberButton);
 
+  let isNecessary = circles.some((circle) =>
+    1 === 1
+    // circle.textContent = numberButton
+    // console.log('подходит кнопка' + circle);
+  );
+  console.log(isNecessary);
 }
 
-
-
+buttons.forEach(button => {
+  button.addEventListener('click', handleClick)
+})
 
 
 
