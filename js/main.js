c8cd4718487902b7b433b271214ac67b83db0a93
Evgene Shlyk <serpupstars@gmail.com>
2024-03-07 07:31:10 +1000

    7 марта утро

--- js/main.js
+++ js/main.js
@@ -1,15 +1,59 @@
-let boxes = document.querySelectorAll('.box');
-let circles = document.querySelectorAll('.circle');
-let templateFragment = document.querySelector('#template').content;
-let template = templateFragment.querySelector('div');
-let fragment = document.createDocumentFragment();
-console.log(boxes[0]);
+const CIRCLE_COLOR = [
+  // 'black',
+  'red',
+  'blue',
+  'yellow',
+  'green',
+  'Magenta',
+  'Aquamarine',
+];
 
-for (let i = 0; i < 6; i++) {
+let boxRender = document.querySelector('.box-render-circle');
+// let template = document.querySelector('#template').content.querySelector('.template__inner');
+let circles = document.querySelectorAll('.circle');
+
+let renderRandomColor = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() *
+    (
+      upper - lower + 1
+    ) +
+    lower;
+
+  return Math.floor(result);
+};
+
+let renderCircles = (event) => {
+  boxRender.innerHTML = '';
+  Array.from({ length: event.currentTarget.textContent }, func);
+};
+
+circles.forEach(circle => circle.addEventListener('click', renderCircles));
+
+let func = () => {
+  let clone = template.cloneNode(true);
+  clone.style.backgroundColor = CIRCLE_COLOR[renderRandomColor(1, 5)];
+
+  boxRender.append(clone);
+};
+
+
+// Находим на странице кнопку и попап
+const button = document.querySelector('.click-button');
+const popup = document.querySelector('.content');
+
+// Навешиваем на кнопку обработчик клика
+function handlePopup(evt) {
+  // Отменяем переход по ссылке
+  evt.preventDefault();
+
+  // Добавляем попапу класс show, делая его видимым
+  popup.classList.add('show');
 
 }
 
-
+button.addEventListener('click', handlePopup);
 
 
 
