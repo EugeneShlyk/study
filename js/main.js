4413067b7edfd5c85b0c44063e25e54836b1cfa7
Evgene Shlyk <serpupstars@gmail.com>
2024-03-19 07:24:00 +1000

    19 марта утро

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// import { handleOpenModal } from './show-modal';
+import '/js/show-modal.js'
 
 const CIRCLE_COLOR = [
   'red',
@@ -39,39 +39,39 @@ let func = () => {
   boxRender.append(clone);
 };
 
-let modalButton = document.querySelector('.open-modal');
-let modal = document.querySelector('.modal');
-let modalCloseButton = document.querySelector('.modal__close-button');
-
-let handleEscapeKeydown = (evt) => {
-  if ( evt.key === 'Escape' ) {
-    evt.preventDefault();
-    modal.classList.add('invisible');
-    modalButton.classList.remove('invisible');
-
-    modalButton.addEventListener('click', handleOpenModal);
-    modalCloseButton.removeEventListener('click', handleCloseModal);
-  }
-}
-
-let handleOpenModal = () => {
-  modal.classList.remove('invisible');
-  modalButton.classList.add('invisible');
-
-  modalButton.removeEventListener('click', handleOpenModal);
-  modalCloseButton.addEventListener('click', handleCloseModal);
-  document.addEventListener('keydown', handleEscapeKeydown);
-}
-
-let handleCloseModal = () => {
-  modal.classList.add('invisible');
-  modalButton.classList.remove('invisible');
-
-  modalButton.addEventListener('click', handleOpenModal);
-  modalCloseButton.removeEventListener('click', handleCloseModal);
-  document.removeEventListener(`keydown`, handleEscapeKeydown);
-}
-
-modalButton.addEventListener('click', handleOpenModal);
+// let modalButton = document.querySelector('.open-modal');
+// let modal = document.querySelector('.modal');
+// let modalCloseButton = document.querySelector('.modal__close-button');
+//
+// let handleEscapeKeydown = (evt) => {
+//   if ( evt.key === 'Escape' ) {
+//     evt.preventDefault();
+//     modal.classList.add('invisible');
+//     modalButton.classList.remove('invisible');
+//
+//     modalButton.addEventListener('click', handleOpenModal);
+//     modalCloseButton.removeEventListener('click', handleCloseModal);
+//   }
+// }
+//
+// let handleOpenModal = () => {
+//   modal.classList.remove('invisible');
+//   modalButton.classList.add('invisible');
+//
+//   modalButton.removeEventListener('click', handleOpenModal);
+//   modalCloseButton.addEventListener('click', handleCloseModal);
+//   document.addEventListener('keydown', handleEscapeKeydown);
+// }
+//
+// let handleCloseModal = () => {
+//   modal.classList.add('invisible');
+//   modalButton.classList.remove('invisible');
+//
+//   modalButton.addEventListener('click', handleOpenModal);
+//   modalCloseButton.removeEventListener('click', handleCloseModal);
+//   document.removeEventListener(`keydown`, handleEscapeKeydown);
+// }
+//
+// modalButton.addEventListener('click', handleOpenModal);
 
 
