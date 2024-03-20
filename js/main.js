d6e51afadb9c36a3cb5b3975001bf2d0bcb27e62
Evgene Shlyk <serpupstars@gmail.com>
2024-03-21 07:21:41 +1000

    21 марта утро

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ import { modalJs } from './show-modal.js';
 
 modalJs();
 
+const Color = {
+  FIREBALLS: [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848',
+  ],
+};
+
 let head = document.querySelector('.template');
 let eyes = head.querySelectorAll('.template__eye');
 
