ee5529bf846b10e03eb5935c6212cf6fe07355b7
Evgene Shlyk <serpupstars@gmail.com>
2024-03-22 07:24:23 +1000

    22 марта утро

--- js/main.js
+++ js/main.js
@@ -15,8 +15,13 @@ const Color = {
 
 let head = document.querySelector('.template');
 let leftEye = head.querySelector('.template__left-eye');
+let leftEyeInput = head.querySelector('[name="color-left-eye"]');
 let rightEye = head.querySelector('.template__right-eye');
-let leftEyeInput = head.querySelector('[name = "color-left-eye"]');
+let rightEyeInput = head.querySelector('[name="color-right-eye"]');
+let nose = head.querySelector('.template__nose');
+let noseInput = head.querySelector('[name="color-nose"]');
+let mouth = head.querySelector('.template__mouth');
+let mouthInput = head.querySelector('[name="color-mouth"]');
 
 
 leftEye.addEventListener('click', (evt) => {
@@ -28,5 +33,17 @@ leftEye.addEventListener('click', (evt) => {
 rightEye.addEventListener('click', (evt) => {
   let randomColor = getRandomArrayElement(Color.FIREBALLS);
   evt.target.style.backgroundColor = randomColor;
-  leftEyeInput.value = randomColor;
+  rightEyeInput.value = randomColor;
+});
+
+nose.addEventListener('click', (evt) => {
+  let randomColor = getRandomArrayElement(Color.FIREBALLS);
+  evt.target.style.backgroundColor = randomColor;
+  noseInput.value = randomColor;
+});
+
+mouth.addEventListener('click', (evt) => {
+  let randomColor = getRandomArrayElement(Color.FIREBALLS);
+  evt.target.style.backgroundColor = randomColor;
+  mouthInput.value = randomColor;
 });
