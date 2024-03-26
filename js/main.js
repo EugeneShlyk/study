a1f51629dfee435bfe9ce82565ab08f5471c4531
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2024-03-26 17:26:38 +1000

    26 марта вечер

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 import { modalJs } from './show-modal.js';
 import { faceColorRender } from './face.js';
 
+let rangeSlider = document.querySelector('#range-slider');
+let minInput = rangeSlider.querySelector('#range-input-min');
+let maxInput = rangeSlider.querySelector('#range-input-max');
+
+if ( rangeSlider ) {
+  noUiSlider.create(rangeSlider, {
+    start: [500, 999999],
+    connect: true,
+    step: 1,
+    range: {
+      'min': [500],
+      'max': [999999]
+    }
+  });
+
+  rangeSlider.noUiSlider.on('update', function () {})
+}
+
 modalJs();
 faceColorRender();
+
+
