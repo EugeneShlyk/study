a958526701cef5ee34bb850b60e8cf8ca6b95de0
Evgene Shlyk <serpupstars@gmail.com>
2024-03-27 07:28:24 +1000

    27 марта утро

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ import { modalJs } from './show-modal.js';
 import { faceColorRender } from './face.js';
 
 let rangeSlider = document.querySelector('#range-slider');
-let minInput = rangeSlider.querySelector('#range-input-min');
-let maxInput = rangeSlider.querySelector('#range-input-max');
+let minInput = document.querySelector('#range-input-min');
+let maxInput = document.querySelector('#range-input-max');
+let inputs = [minInput, maxInput];
+
+
+
+modalJs();
+faceColorRender();
 
 if ( rangeSlider ) {
   noUiSlider.create(rangeSlider, {
@@ -16,10 +22,21 @@ if ( rangeSlider ) {
     }
   });
 
-  rangeSlider.noUiSlider.on('update', function () {})
+  rangeSlider.noUiSlider.on('update', function (values, handle) {
+    inputs[handle].value = Math.round(values[handle]);
+  });
+
+  let setRangeSlider = (index, value) => {
+    let array = [null, null];
+    array[index] = value;
+
+    console.log(array);
+  }
+
+  inputs.forEach((item, index) => {
+    item.addEventListener('change', (evt) => {
+      console.log(index);
+      setRangeSlider(index, evt.currentTarget.value)
+    });
+  });
 }
-
-modalJs();
-faceColorRender();
-
-
